019776f11339f6eb68cd4c791c099f305dcb5336
Replit user <>
2022-08-11 15:36:15 -0300

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const formidable = require('formidable')
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +12,21 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', (req, res) => {
+  const form = formidable({multiples: true})
 
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    const name = files.upfile.originalFilename
+    const type = files.upfile.mimetype
+    const size = files.upfile.size
+    
+    res.json({ name: name, type: type, size: size });
+  });
+})
 
 
 const port = process.env.PORT || 3000;
